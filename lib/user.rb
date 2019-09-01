c144a7b58b60008edd7df5c6ac7b3db4914a29b8
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 01:08:44 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User 
   attr_accessor :firstName, :lastName
   
-  def initialize (firstName, lastName)
+  def initialize(firstName, lastName)
     @firstName = firstName
     @lastName = lastName
   end
