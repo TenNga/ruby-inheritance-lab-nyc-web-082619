72303b34281cddd6b5a6eac697ec422a79e8185c
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 01:18:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,16 @@
 class Student < User 
   
+  attr_reader : knowledge
+  
   def initialize
     @knowledge = []
   end
   
   def learn(string)
-    @knowledge << string
+    knowledge << string
   end 
 
+  def knowledge
+    knowledge
+  end
 end
